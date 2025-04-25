10056a8ab4181e3996436711cd7b56521f3f88f5
unisanta <ls235182@alunos.unisanta.br>
2025-04-25 20:39:45 -0300

    feat: adicao do tp 08 da aula de hoje.

---
+++ TP 08/Cinemark/Cinemark/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Cinemark
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFaturamento = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFaturamento
+            // 
+            this.btnFaturamento.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnFaturamento.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFaturamento.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFaturamento.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFaturamento.ForeColor = System.Drawing.Color.White;
+            this.btnFaturamento.Location = new System.Drawing.Point(48, 463);
+            this.btnFaturamento.Name = "btnFaturamento";
+            this.btnFaturamento.Size = new System.Drawing.Size(157, 62);
+            this.btnFaturamento.TabIndex = 0;
+            this.btnFaturamento.Text = "Faturamento total";
+            this.btnFaturamento.UseVisualStyleBackColor = false;
+            this.btnFaturamento.Click += new System.EventHandler(this.btnFaturamento_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.btnFaturamento);
+            this.Name = "Form1";
+            this.Text = "Cinemark do Asenjo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFaturamento;
+    }
+}
+
---
+++ TP 08/Cinemark/Cinemark/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cinemark
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int largura = 40;
+            int altura = 40;
+            int espaco = 5;
+
+            string[] linhas = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+
+            for (int i = 0; i < linhas.Length; i++)
+            {
+                for (int j = 1; j <= 20; j++)
+                {
+                    Button cadeira = new Button();
+                    cadeira.Width = largura;
+                    cadeira.Height = altura;
+                    cadeira.Left = j * (largura + espaco);
+                    cadeira.Top = i * (altura + espaco);
+                    cadeira.Text = linhas[i] + j;
+                    cadeira.BackColor = Color.Green;
+                    cadeira.ForeColor = Color.White;
+                    cadeira.Cursor = Cursors.Hand;
+
+                    cadeira.FlatStyle = FlatStyle.Flat;
+                    cadeira.FlatAppearance.BorderSize = 1;
+                    cadeira.Font = new Font("Arial", 9, FontStyle.Bold);
+
+                    cadeira.Click += Cadeira_Click;
+
+                    this.Controls.Add(cadeira);
+                }
+            }
+    
+        }
+        private void Cadeira_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if (btn.BackColor == Color.Green)
+            {
+                DialogResult resposta = MessageBox.Show(
+                    "Deseja reservar a cadeira " + btn.Text + "?",
+                    "Confirmação",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question
+                );
+
+                if (resposta == DialogResult.Yes)
+                {
+                    btn.BackColor = Color.Red;
+                }
+            }
+            else if (btn.BackColor == Color.Red)
+            {
+                btn.BackColor = Color.Green;
+            }
+        }
+
+
+        private void btnFaturamento_Click(object sender, EventArgs e)
+        {
+            int ocupadas = 0;
+
+            foreach (Control controle in this.Controls)
+            {
+                if (controle is Button)
+                {
+                    Button cadeira = controle as Button;
+
+                    if (cadeira.BackColor == Color.Red && cadeira.Text != "Faturamento")
+                    {
+                        ocupadas++;
+                    }
+                }
+            }
+
+            int faturamento = ocupadas * 20;
+            MessageBox.Show("Faturamento total: R$ " + faturamento, "Bilheteria");
+        }
+
+    }
+}
+
